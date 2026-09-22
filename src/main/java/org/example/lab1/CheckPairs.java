7ec710ed4ad8978e742351db519b04c54475c7cc
Gervil-sys <catbravl20hxjs@gmail.com>
2026-09-22 13:12:01 +0600

    Добавил комментарий

--- src/main/java/org/example/lab1/CheckPairs.java
+++ src/main/java/org/example/lab1/CheckPairs.java
@@ -4,34 +4,38 @@ import java.util.Map;
 import java.util.Stack;
 
 public class CheckPairs {
+    //Метод
     public static boolean isBalabce(String str){
+        //Создания Стэка
         Stack<Character> stack = new Stack<>();
-
+        //Создание Мап и добавление скобок
         Map<Character, Character> pairs = new HashMap<>();
         pairs.put(')', '(');
         pairs.put(']', '[');
         pairs.put('}', '{');
-
+        //Проходим по символу
         for (int i = 0; i < str.length(); i++){
             char current = str.charAt(i);
-
+            //Если символ есть в Мап, то это закрывающийся
             if (pairs.containsKey(current)) {
-                if (stack.isEmpty()) {
+                if (stack.isEmpty()) { //Стэк пуст
                     return  false;
                 }
-
+                //Вверхний элемент стэка
                 char top = stack.peek();
-
+                //Проверяем свопадение с открывающей и закрывающей
                 if (top == pairs.get(current)) {
-                    stack.pop();
+                    stack.pop(); //Да
                 } else {
-                    return false;
+                    return false; //Нет
                 }
             }
+            //Открывающая идет в стэк
             else {
                 stack.push(current);
             }
         }
+        //Стэк пуст
         return stack.isEmpty();
     }
 }
