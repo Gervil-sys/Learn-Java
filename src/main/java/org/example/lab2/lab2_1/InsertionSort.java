2d11358d3d6e66c1b97ad4a6c9cf86be570a0c2f
Gervil-sys <catbravl20hxjs@gmail.com>
2026-09-22 14:59:53 +0600

    Лаб 2.1. Переписал шаблон

--- src/main/java/org/example/lab2/lab2_1/InsertionSort.java
+++ src/main/java/org/example/lab2/lab2_1/InsertionSort.java
@@ -1,4 +1,10 @@
 package org.example.lab2.lab2_1;
+import java.util.List;
 
-public class InsertionSort {
+public class InsertionSort implements  Sorting<Integer> {
+    @Override
+    public void sort(List<Integer> nums) {
+
+    }
+
 }
--- src/main/java/org/example/lab2/lab2_1/Sorting.java
+++ src/main/java/org/example/lab2/lab2_1/Sorting.java
@@ -1,4 +1,6 @@
 package org.example.lab2.lab2_1;
+import java.util.List;
 
-public class Sorting {
+public interface Sorting<T extends Comparable<T>>{
+    void sort(List<T> nums);
 }
